5e5b32d0eaf297569623a78d48ed59e727715b8f
Ken Gorab <ken.gorab@gmail.com>
2017-07-14 21:05:21 -0400

    Began REPL; pulled in jline to make it possible

---
+++ src/main/java/co/kenrg/mega/Main.java
@@ -0,0 +1,9 @@
+package co.kenrg.mega;
+
+import co.kenrg.mega.repl.Repl;
+
+public class Main {
+    public static void main(String[] args) {
+        Repl.start();
+    }
+}
---
+++ src/main/java/co/kenrg/mega/repl/Repl.java
@@ -0,0 +1,33 @@
+package co.kenrg.mega.repl;
+
+import co.kenrg.mega.lexer.Lexer;
+import co.kenrg.mega.token.Token;
+import co.kenrg.mega.token.TokenType;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+
+public class Repl {
+
+    public static void start() {
+        LineReader reader = LineReaderBuilder.builder().build();
+        String prompt = ">> ";
+
+        while (true) {
+            try {
+                String line = reader.readLine(prompt);
+                Lexer lexer = new Lexer(line);
+
+                Token token = lexer.nextToken();
+                while (token.type != TokenType.EOF) {
+                    System.out.println(token.literal);
+                    token = lexer.nextToken();
+                }
+            } catch (UserInterruptException | EndOfFileException e) {
+                System.out.println("Bye for now!");
+                return;
+            }
+        }
+    }
+}
