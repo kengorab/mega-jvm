d5404c04d7d027942300a5dc9da1d55795f123cc
Ken Gorab <ken.gorab@gmail.com>
2017-08-08 19:47:16 -0400

    Added command line options, and ability to read and evaluate file

--- src/main/java/co/kenrg/mega/Main.java
+++ src/main/java/co/kenrg/mega/Main.java
@@ -1,9 +1,66 @@
 package co.kenrg.mega;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import co.kenrg.mega.repl.Repl;
+import co.kenrg.mega.repl.evaluator.Environment;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
 
 public class Main {
-    public static void main(String[] args) {
-        Repl.start();
+    public static void main(String[] args) throws ParseException {
+        Options opts = new Options();
+        opts.addOption("r", "repl", false, "Evaluates the given file, and loads a REPL");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine command;
+        try {
+            command = parser.parse(opts, args);
+        } catch (UnrecognizedOptionException e) {
+            printUsage(opts);
+            System.exit(1);
+            return;
+        }
+        List<String> argList = command.getArgList();
+
+        if (argList.size() == 0) {
+            Repl.start(new Environment());
+            return;
+        }
+
+        if (argList.size() == 1) {
+            Environment env = new Environment();
+            Path filepath = Paths.get(argList.get(0));
+            try {
+                byte[] bytes = Files.readAllBytes(filepath);
+                String code = new String(bytes);
+                Repl.readEvalPrint(code, env);
+            } catch (IOException e) {
+                System.err.printf("No such file: %s\n", filepath.toAbsolutePath().toString());
+                System.exit(1);
+            }
+
+            if (command.hasOption('r')) {
+                Repl.start(env);
+            }
+
+            return;
+        }
+
+        printUsage(opts);
+    }
+
+    private static void printUsage(Options opts) {
+        HelpFormatter usage = new HelpFormatter();
+        usage.printHelp("mega [filename]", opts);
     }
 }
--- src/main/java/co/kenrg/mega/repl/Repl.java
+++ src/main/java/co/kenrg/mega/repl/Repl.java
@@ -14,33 +14,35 @@ import org.jline.reader.UserInterruptException;
 
 public class Repl {
 
-    public static void start() {
+    public static void start(Environment env) {
         LineReader reader = LineReaderBuilder.builder()
             .build();
         reader.setOpt(Option.DISABLE_EVENT_EXPANSION);
         String prompt = ">> ";
 
-        Environment env = new Environment();
-
         while (true) {
             try {
                 String line = reader.readLine(prompt);
-                Lexer lexer = new Lexer(line);
-                Parser parser = new Parser(lexer);
-                Module module = parser.parseModule();
-
-                if (!parser.errors.isEmpty()) {
-                    System.out.println("There were errors!!!");
-                }
-
-                Obj evaluated = Evaluator.eval(module, env);
-                if (evaluated != null) {
-                    System.out.println(evaluated.inspect(0));
-                }
+                readEvalPrint(line, env);
             } catch (UserInterruptException | EndOfFileException e) {
                 System.out.println("Bye for now!");
                 return;
             }
+        }
+    }
+
+    public static void readEvalPrint(String code, Environment env) {
+        Lexer lexer = new Lexer(code);
+        Parser parser = new Parser(lexer);
+        Module module = parser.parseModule();
+
+        if (!parser.errors.isEmpty()) {
+            System.out.println("There were errors!!!");
+        }
+
+        Obj evaluated = Evaluator.eval(module, env);
+        if (evaluated != null) {
+            System.out.println(evaluated.inspect(0));
         }
     }
 }
